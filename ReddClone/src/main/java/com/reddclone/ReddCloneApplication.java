ef61e6df3fbbfc3401dd248b62d08a61aeafe9f0
Rares Lup <luprares@gmail.com>
2021-05-21 18:32:16 +0300

    initial check-in of the ReddClone code

---
+++ ReddClone/src/main/java/com/reddclone/ReddCloneApplication.java
@@ -0,0 +1,13 @@
+package com.reddclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddCloneApplication.class, args);
+	}
+
+}
---
+++ ReddClone/src/test/java/com/reddclone/ReddCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
