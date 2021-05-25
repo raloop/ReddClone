404a80d12fb215dc13b639e6026e917d4e6667cb
Rares Lup <luprares@gmail.com>
2021-05-25 14:37:03 +0300

    creating custom login page

--- ReddClone/src/main/java/com/reddclone/web/LoginController.java
+++ ReddClone/src/main/java/com/reddclone/web/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
-
-	@GetMapping("/")
-	public String rootView() {
-		return "index";
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
