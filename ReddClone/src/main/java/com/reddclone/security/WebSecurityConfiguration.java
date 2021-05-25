7c73ad11f4c341a331dc0220bf47e53bf2dde4db
Rares Lup <luprares@gmail.com>
2021-05-25 16:06:48 +0300

    enabling CSRF protection

--- ReddClone/src/main/java/com/reddclone/security/WebSecurityConfiguration.java
+++ ReddClone/src/main/java/com/reddclone/security/WebSecurityConfiguration.java
@@ -22,13 +22,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http
 			.authorizeRequests()
 			  .antMatchers("/").permitAll()
 			  .anyRequest().hasRole("USER")
 			.and()
 			.formLogin()
 			  .loginPage("/login")
+			  .defaultSuccessUrl("/dashboard")
 			  .permitAll()
 			  .and()
 			.logout()
