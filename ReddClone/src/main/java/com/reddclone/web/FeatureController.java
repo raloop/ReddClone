b1fa43859cfdbb3a03449f9f13ca5046bbe5af49
Rares Lup <luprares@gmail.com>
2021-06-10 16:40:45 +0300

    fixing bug in feature controller

--- ReddClone/src/main/java/com/reddclone/web/FeatureController.java
+++ ReddClone/src/main/java/com/reddclone/web/FeatureController.java
@@ -1,23 +1,32 @@
 package com.reddclone.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.reddclone.domain.Feature;
 import com.reddclone.service.FeatureService;
 
 
+@Controller
 @RequestMapping("/products/{productId}/features")
 public class FeatureController {
 	
 	@Autowired
 	private FeatureService featureService;
 	
-	@PostMapping
+	@PostMapping("")
 	public String createFeature(@PathVariable Long productId) {
-		featureService.createFeature(productId);
+		Feature feature = featureService.createFeature(productId);
 		
+		return "redirect:/products/"+productId+"/features/"+feature.getId();
+	}
+	
+	@GetMapping("/{featureId}")
+	public String getFeature(@PathVariable Long productId, @PathVariable Long featureId) {
 		return "feature";
 	}
 }
